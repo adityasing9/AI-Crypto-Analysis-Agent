9fbf951a8590398b09fdcef67187bdbd1914be2d
adityasing9 <to.msg.aadi@gmail.com>
2026-03-31 16:24:21 +0530

    fixed api key for streamlit deployment

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import os
+import streamlit as st
 import requests
 import pandas as pd
 from openai import OpenAI
@@ -8,9 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1", api_key=os.getenv("OPENROUTER_API_KEY")
-)
+client = OpenAI(base_url="https://openrouter.ai/api/v1", api_key=api_key)
 
 # ---------------- DATA ---------------- #
 
